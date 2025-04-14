d86d611f8245a7d38ab1fd3ae7ec1d5f678d200f
Dylan-Gerard <d.marcgerard@gmail.com>
2025-04-14 09:46:44 +0200

    redeploy

--- src/services/TranslationService.tsx
+++ src/services/TranslationService.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_BASE_URL =
     process.env.NODE_ENV === 'development'
         ? 'http://localhost:3001'
-        : 'https://app-translate-novel.onrender.com';
+        : 'https://app-translate-novel.onrender.com/translate';
 
 export const translateText = async (text: string, targetLang: string) => {
     try {
